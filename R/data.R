bb9fcf296d8154a93d023aae0ae9c702a4f95a29
ondrej-klima <ondra.klima@seznam.cz>
2023-10-15 12:32:08 +0200

    Added loading data.

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Example fires data
+#' 
+#' Contains count of fires w.r.t date and area. 
+#' 
+"fires"
+
+#' Example weather data
+#' 
+#' Contains weather data w.r.t date and area. 
+#' 
+"weather"
+
+#' Example joined data
+#' 
+#' Contains weather data joined with fire counts. 
+#' 
+"joined"
