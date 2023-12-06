7feb8fdeec6cedcbe63a677aa373f5bca1ea7b68
ondrej-klima <ondra.klima@seznam.cz>
2023-12-06 12:15:40 +0100

    Added sliders to Saaty matrix evaluation.

--- R/info.R
+++ R/info.R
@@ -19,7 +19,103 @@ infoUi <- function(id) {
                              htmltools::tags$style('#myid2 * { word-wrap: break-word; }'),
                              htmltools::div(id='myid2', rhandsontable::rHandsontableOutput(
                                shiny::NS(id, "table1"))
-                               )
+                               ),
+                             shiny::fluidPage(
+                               shiny::fluidRow(
+                                 shiny::column(6,
+                             shiny::fluidPage(
+                               shiny::fluidRow(
+                                 shiny::column(3, 'Dostupnost dat'),
+                                 shiny::column(6,
+                                   shinyWidgets::sliderTextInput(
+                                    inputId = NS(id, "slider1"),
+                                    label = NULL,
+                                    grid = TRUE,
+                                    force_edges = TRUE,
+                                    choices = 1:9,
+                                    selected = 8
+                                   )
+                                 ),
+                                 shiny::column(3, 'Věrohodnost dostupných dat'),
+                               ),
+                               shiny::fluidRow(
+                               shiny::column(3, 'Dostupnost dat'),
+                               shiny::column(6,
+                                             shinyWidgets::sliderTextInput(
+                                               inputId = NS(id, "slider2"),
+                                               label = NULL,
+                                               grid = TRUE,
+                                               force_edges = TRUE,
+                                               choices = 1:9,
+                                               selected = 3
+                                             )
+                               ),
+                               shiny::column(3, 'Aktuálnost dostupných dat'),
+                             ),
+                             shiny::fluidRow(
+                               shiny::column(3, 'Dostupnost dat'),
+                               shiny::column(6,
+                                             shinyWidgets::sliderTextInput(
+                                               inputId = NS(id, "slider3"),
+                                               label = NULL,
+                                               grid = TRUE,
+                                               force_edges = TRUE,
+                                               choices = 1:9,
+                                               selected = 2
+                                             )
+                               ),
+                               shiny::column(3, 'Míra vlivu prediktoru'),
+                             ),
+                             shiny::fluidRow(
+                                shiny::column(3, 'Věrohodnost dostupných dat'),
+                                shiny::column(6,
+                                              shinyWidgets::sliderTextInput(
+                                                inputId = NS(id, "slider4"),
+                                                label = NULL,
+                                                grid = TRUE,
+                                                force_edges = TRUE,
+                                                choices = 1:9,
+                                                selected = 3
+                                              )
+                                ),
+                                shiny::column(3, 'Aktuálnost dostupných dat'),
+                              ),
+                              shiny::fluidRow(
+                                shiny::column(3, 'Věrohodnost dostupných dat'),
+                                shiny::column(6,
+                                              shinyWidgets::sliderTextInput(
+                                                inputId = NS(id, "slider5"),
+                                                label = NULL,
+                                                grid = TRUE,
+                                                force_edges = TRUE,
+                                                choices = 1:9,
+                                                selected = 4
+                                              )
+                                ),
+                                shiny::column(3, 'Míra vlivu prediktoru'),
+                              )
+                             ),
+                             shiny::fluidRow(
+                               shiny::column(3, 'Aktuálnost dostupných dat'),
+                               shiny::column(6,
+                                             shinyWidgets::sliderTextInput(
+                                               inputId = NS(id, "slider6"),
+                                               label = NULL,
+                                               grid = TRUE,
+                                               force_edges = TRUE,
+                                               choices = 1:9,
+                                               selected = 2
+                                             )
+                               ),
+                               shiny::column(3, 'Míra vlivu prediktoru'),
+                             )),
+                             shiny::column(6,shiny::tableOutput(shiny::NS(id, 'saaty')
+
+                             )
+                                           )
+
+                             ))
+
                            ),
                            shiny::tabPanel('Registr prediktorů',
                              htmltools::tags$style('#myid3 * { word-wrap: break-word; }'),
@@ -55,62 +151,111 @@ infoServer <- function(id) {
       )
     })
 
-    si <- Saaty()
+    saatyMatrix <- reactiveVal()
+    si <- reactiveVal()
+
+    observe({
+      saatyMatrix(matrix(c(1,input$slider1,input$slider2,input$slider3,
+               1/input$slider1,1,1/input$slider4,1/input$slider5,
+               1/input$slider2,input$slider4,1,1/input$slider6,
+               1/input$slider3,input$slider5,input$slider6,1),4,4))
+      si(Saaty(saatyMatrix()))
+
+      if(!is.null(input$table1)) {
+      df = rhandsontable::hot_to_r(input$table1)
+      for(i in seq(dim(df)[1])) {
+        vals <- c(
+          df$availability[i],
+          df$realibility[i],
+          df$recentness[i],
+          df$impact[i]
+        )
+        row <- c(
+          which(availability == vals[1]) - 1,
+          which(realibility == vals[2]) - 1,
+          which(recentness == vals[3]),
+          which(impact == vals[4])
+        )
+        df$evaluation[i] = if(!any(is.na(vals))) {
+          sum(si() * row)
+        }
+        else { as.numeric(NA) }
+      }
+      data(df)
+      }
+
+
+    })
+
+    output$saaty <- shiny::renderTable({
+
+      A<-saatyMatrix()
+
+      colnames(A)<-c("Dost.", "Věr.", "Akt.", "Míra")
+
+    rownames(A)<-c("Dostupnost dat",
+                                "Věrohodnost dostupných dat",
+                                "Aktuálnost dostupných dat",
+                                "Odhadovaná míra předpokládatného ovlivnění hodnot vysvětlované proměnné")
+    A
+    }, rownames = TRUE)
+
+
 
     availability <- c(
       paste0(
-        'Data nejsou dostupná a nelze je opatřit ani s vynaložením ',
+        '0: Data nejsou dostupná a nelze je opatřit ani s vynaložením ',
         'značného úsilí.'),
-      'Data nejsou dostupná, ale lze je opatřit s vynaložením značného úsilí.',
+      '1: Data nejsou dostupná, ale lze je opatřit s vynaložením značného úsilí.',
       paste0(
-        'Data nejsou dostupná, ale lze je opatřit s vynaložením přijatelného ',
+        '2: Data nejsou dostupná, ale lze je opatřit s vynaložením přijatelného ',
         'úsilí v rámci standardních činností.'),
       paste0(
-        'Data jsou dostupná u externího poskytovatele nebo v rámci otevřených ',
-        'zdrojů, kde je lze snadno opatřit.',
-        'Data jsou dostupná ve vlastních databázích.'
-      ))
+        '3: Data jsou dostupná u externího poskytovatele nebo v rámci otevřených ',
+        'zdrojů, kde je lze snadno opatřit.'),
+        '4: Data jsou dostupná ve vlastních databázích.'
+      )
 
     realibility <- c(
-      'Metodika získávání dat není známa.',
+      '0: Metodika získávání dat není známa.',
       paste0(
-        'Metodika získávání dat je známa, avšak vzhledem k nastavení této ',
+        '1: Metodika získávání dat je známa, avšak vzhledem k nastavení této ',
         'metodiky lze předpokládat, že data jsou do značné míry nespolehlivá.'),
       paste0(
-        'Metodika získávání dat je známa, avšak vzhledem k nastavení této ',
+        '2: Metodika získávání dat je známa, avšak vzhledem k nastavení této ',
         'metodiky lze předpokládat, že data mohou být v omezené míře ',
         'nespolehlivá.'),
       paste0(
-        'Metodika získávání dat je známa, avšak vzhledem k nastavení této ',
+        '3: Metodika získávání dat je známa, avšak vzhledem k nastavení této ',
         'metodiky nelze vyloučit, že data mohou být v omezené míře ',
         'nespolehlivá.'),
       paste0(
-        'Metodika získávání dat je známa a vzhledem k jejímu nastavení lze ',
+        '4: Metodika získávání dat je známa a vzhledem k jejímu nastavení lze ',
         'předpokládat, že dostupná data jsou spolehlivá (např. přístrojové ',
         'měření).'
       ))
 
     recentness <- c(
       paste0(
-        'Data jsou k dispozici s krátkou prodlevou, která výrazně ',
+        '1: Data jsou k dispozici s krátkou prodlevou, která výrazně ',
         'převyšuje časový interval pro průměrování dle čl. 3 odst. 2 písm. d) ',
         'metodického postupu.'),
       paste0(
-        'Data jsou k dispozici s krátkou prodlevou, která mírně převyšuje ',
+        '2: Data jsou k dispozici s krátkou prodlevou, která mírně převyšuje ',
         'časový interval pro průměrování dle čl. 3 odst. 2 písm. d) ',
         'metodického postupu.'),
       paste0(
-        'Data jsou k dispozici s krátkou prodlevou, která nepřevyšuje časový ',
+        '3: Data jsou k dispozici s krátkou prodlevou, která nepřevyšuje časový ',
         'interval pro průměrování dle čl. 3 odst. 2 písm. d) metodického ',
         'postupu.'),
-      'Data jsou k dispozici ihned (např. měření a záznam dat v reálném čase).'
+      '4: Data jsou k dispozici ihned (např. měření a záznam dat v reálném čase).'
     )
 
     impact <- c(
-      'Prediktor v malé míře ovlivňuje hodnoty predikované proměnné.',
-      'Prediktor v nemalé míře ovlivňuje hodnoty predikované proměnné.',
-      'Prediktor výrazně ovlivňuje hodnoty predikované proměnné.',
-      'Prediktor rozhodujícím způsobem ovlivňuje hodnoty predikované proměnné.'
+      '1: Prediktor v malé míře ovlivňuje hodnoty predikované proměnné.',
+      '2: Prediktor v nemalé míře ovlivňuje hodnoty predikované proměnné.',
+      '3: Prediktor výrazně ovlivňuje hodnoty predikované proměnné.',
+      '4: Prediktor rozhodujícím způsobem ovlivňuje hodnoty predikované proměnné.'
     )
 
     cnames <- c(
@@ -167,7 +312,7 @@ infoServer <- function(id) {
         contextMenu = TRUE,
         stretchH = "all",
         width = '100%',
-        height = 800,
+        height = 400,
         colWidths = c(150, 150, 150, 150, 150, 60, 60),
         manualColumnResize = TRUE,
         manualRowResize = TRUE,
@@ -239,7 +384,7 @@ infoServer <- function(id) {
           which(impact == vals[4])
         )
         df$evaluation[i] = if(!any(is.na(vals))) {
-          sum(si * row)
+          sum(si() * row)
         }
         else { as.numeric(NA) }
       }
